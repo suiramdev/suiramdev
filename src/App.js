cbb40a744dada5ec4e88472d435d22878fa0421b
HamsterOh <mariusnouchet@gmail.com>
2020-11-24 00:29:44 +0100

    Blog init

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.scss';
+import Blog from 'screens/Blog';
+
+function App() {
+    return (
+        <div className="app">
+            <Blog />
+        </div>
+    );
+}
+
+export default App;
---
+++ src/screens/Blog.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import './Blog.scss';
+import { motion } from 'framer-motion';
+import CreateIcon from '@material-ui/icons/Create';
+import SportsEsportsIcon from '@material-ui/icons/SportsEsports';
+import CodeIcon from '@material-ui/icons/Code';
+import MenuBookIcon from '@material-ui/icons/MenuBook';
+import { Avatar } from '@material-ui/core';
+import { GitHub, LinkedIn, Twitter, VideogameAsset } from '@material-ui/icons';
+
+export default class Blog extends Component {
+    render() {
+        return (
+            <div className="blog">
+                <a className="blog-edit">
+                    <CreateIcon />
+                </a>
+
+                <div className="blog__sections">
+                    <section>
+                        <h1>2020</h1>
+                        <a className="active"><SportsEsportsIcon /> My first s&box project</a>
+                        <a><MenuBookIcon /> Watch Dogs Legion review</a>
+                        <a><CodeIcon /> ReactJS and Firebase</a>
+                        <a><CodeIcon /> Food delivery</a>
+                    </section>
+                    <section>
+                        <h1>2017</h1>
+                        <a><SportsEsportsIcon /> My first game jam !</a>
+                    </section>
+                    <div className="blog__sections-medias">
+                        <a href="https://github.com/suiramdev"><GitHub /></a>
+                        <a href="https://twitter.com/suiramdev"><Twitter /></a>
+                        <a href="https://suiramdev.itch.io"><VideogameAsset /></a>
+                    </div>
+                </div>
+                <div className="blog__content">
+                    <p>Biography
+                    Born in 2002, I 'm passionate in game developement since 4 years… I started developing games on Unity for my own and i'
+                    m now looking to turn this passion into my real Job.I already made some games, websites and mods that you can have a look on my website.
+
+                    My knowledges </p>
+                </div>
+            </div>
+        )
+    }
+}
