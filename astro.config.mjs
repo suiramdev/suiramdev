b133f0042d977c4a740e6a68cd06cb331465afbd
Marius Nouchet <suiramdev@Mariuss-MacBook-Air.local>
2022-12-19 16:51:36 +0100

    :heavy_minus_sign: Removed astrojs-sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,15 +4,12 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
-import sitemap from "@astrojs/sitemap";
-
-// https://astro.build/config
 import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  output: "server",
-  site: "http://localhost:3000",
-  integrations: [tailwind(), sitemap()],
-  adapter: vercel()
+    output: "server",
+    site: "http://localhost:3000",
+    integrations: [tailwind()],
+    adapter: vercel()
 });
