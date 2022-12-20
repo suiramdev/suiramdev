cfd676052a763fad5d2c1d78ab3b84103b879fb4
Marius Nouchet <suiramdev@Mariuss-MacBook-Air.local>
2022-12-20 15:28:44 +0100

    :wip: Try building website for netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,13 +3,14 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 import tailwind from "@astrojs/tailwind";
 
+
 // https://astro.build/config
-import vercel from "@astrojs/vercel/serverless";
+import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
     output: "server",
     site: "http://localhost:3000",
     integrations: [tailwind()],
-    adapter: vercel()
+    adapter: netlify()
 });
