a9b9abf0c7024b5ec508469906fd2276211029cb
Marius Nouchet <suiramdev@Mariuss-MacBook-Air.local>
2022-12-22 18:44:17 +0100

    :sparkles: Contact section

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
+    content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+    theme: {
+        extend: {
+            backgroundImage: {
+                'grid-pattern': 'linear-gradient(to bottom, rgba(0, 0, 0 , 0.8), transparent, transparent), url(/grid.svg)',
+            },
+        },
+    },
+    plugins: [],
 }
