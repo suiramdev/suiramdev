fcef590cfd5bb09e683ff53683b52eab60e879d9
suiramdev <mariusnouchet@gmail.com>
2023-01-08 00:33:44 +0100

    :bug: Attempt AGAIIIIN to fix the rendering problem on prod

--- src/pages/api/og.png.ts
+++ src/pages/api/og.png.ts
@@ -8,8 +8,10 @@ await initWasm(fetch('https://unpkg.com/@resvg/resvg-wasm/index_bg.wasm'));
 export async function get({ request }: any) {
     const params = new URL(request.url).searchParams;
 
-    const svg = await satori(
-        html(`
+    try {
+
+        const svg = await satori(
+            html(`
             <div tw="w-full h-full flex bg-black">
                 <div style="font-family: 'Inter'; background-image: linear-gradient(to bottom, rgba(0, 0, 0 , 0.8), transparent, transparent), url(${new URL("grid.svg", import.meta.env.SITE)});" tw="w-full h-full px-24 flex flex-col justify-center items-center text-center">
                    <span class="text-9xl">${params.get("icon") || ""}</span>
@@ -18,57 +20,62 @@ export async function get({ request }: any) {
                 </div>
             </div>
         `),
-        {
-            width: 1200,
-            height: 630,
-            fonts: [
-                {
-                    name: 'Inter',
-                    data: await (await fetch(
-                        new URL(`/og/Inter-Regular.ttf`, import.meta.env.SITE)
-                    )).arrayBuffer(),
-                    weight: 400,
-                },
-                {
-                    name: 'Inter',
-                    data: await (await fetch(
-                        new URL(`/og/Inter-Medium.ttf`, import.meta.env.SITE)
-                    )).arrayBuffer(),
-                    weight: 500,
-                },
-                {
-                    name: 'Inter',
-                    data: await (await fetch(
-                        new URL(`/og/Inter-Bold.ttf`, import.meta.env.SITE)
-                    )).arrayBuffer(),
-                    weight: 700,
-                },
-            ],
-            loadAdditionalAsset: async (code: string, segment: string) => {
-                if (code === "emoji") {
-                    return "data:image/svg+xml;base64," +
-                        btoa(await loadEmoji("fluent", getIconCode(segment)))
-                }
+            {
+                width: 1200,
+                height: 630,
+                fonts: [
+                    {
+                        name: 'Inter',
+                        data: await (await fetch(
+                            new URL(`/og/Inter-Regular.ttf`, import.meta.env.SITE)
+                        )).arrayBuffer(),
+                        weight: 400,
+                    },
+                    {
+                        name: 'Inter',
+                        data: await (await fetch(
+                            new URL(`/og/Inter-Medium.ttf`, import.meta.env.SITE)
+                        )).arrayBuffer(),
+                        weight: 500,
+                    },
+                    {
+                        name: 'Inter',
+                        data: await (await fetch(
+                            new URL(`/og/Inter-Bold.ttf`, import.meta.env.SITE)
+                        )).arrayBuffer(),
+                        weight: 700,
+                    },
+                ],
+                loadAdditionalAsset: async (code: string, segment: string) => {
+                    if (code === "emoji") {
+                        return "data:image/svg+xml;base64," +
+                            btoa(await loadEmoji("fluent", getIconCode(segment)))
+                    }
 
-                return (code);
+                    return (code);
+                },
             },
-        },
-    );
+        );
 
-    const resvgJS = new Resvg(svg, {
-        fitTo: {
-            mode: 'width',
-            value: 1200,
-        },
-    });
-    const image = resvgJS.render();
+        const resvgJS = new Resvg(svg, {
+            fitTo: {
+                mode: 'width',
+                value: 1200,
+            },
+        });
+        const image = resvgJS.render();
 
-    return {
-        headers: {
-            'Content-Type': 'image/png',
-            'Cache-Control': 'public, max-age=31536000, immutable'
-        },
-        body: image.asPng(),
-        encoding: "binary",
-    };
+        return new Response(image.asPng(), {
+            headers: {
+                "Content-Type": "image/png;charset=utf-8",
+                "Cache-Control": "public, max-age=31536000, immutable",
+            },
+        });
+    } catch (e: any) {
+        console.error(e.message);
+        return new Response(null, {
+            status: 500,
+            statusText: "Something went wrong..."
+        });
+    }
 }
