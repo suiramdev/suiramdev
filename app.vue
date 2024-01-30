c49ad1f28c6b4b91bd772f0f15a9ec96de949c31
suiramdev <me@suiram.dev>
2024-01-30 04:11:56 +0100

    :chart_with_upwards_trend: Added Vercel speed insights and analytics

--- app.vue
+++ app.vue
@@ -1,8 +1,13 @@
 <template>
   <div>
+    <SpeedInsights />
     <AppSideBar />
     <div class="md:ml-60">
       <NuxtPage />
     </div>
   </div>
 </template>
+
+<script setup lang="ts">
+import { SpeedInsights } from "@vercel/speed-insights/nuxt";
+</script>
